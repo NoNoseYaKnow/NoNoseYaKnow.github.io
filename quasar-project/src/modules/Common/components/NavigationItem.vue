7d48e07e8a1ca1e6d504729e1daa24b831153496
Josh Manjaro <josh.burchdco@gmail.com>
2022-10-18 19:53:33 -0500

    initial commit of Common NavigationItem component

---
+++ quasar-project/src/modules/Common/components/NavigationItem.vue
@@ -0,0 +1,23 @@
+<template>
+  <q-item v-ripple clickable active-class="active-link">
+    <!-- Icon -->
+    <q-item-section avatar class="q-pl-sm">
+      <q-icon :name="icon"></q-icon>
+    </q-item-section>
+    <!-- Label -->
+    <q-item-section>{{ label }}</q-item-section>
+  </q-item>
+</template>
+
+<script setup lang="ts">
+export interface NavigationItemProps {
+  icon?: string;
+  label?: string;
+}
+withDefaults(defineProps<NavigationItemProps>(), {
+  icon: "",
+  label: "",
+});
+</script>
+
+<style></style>
