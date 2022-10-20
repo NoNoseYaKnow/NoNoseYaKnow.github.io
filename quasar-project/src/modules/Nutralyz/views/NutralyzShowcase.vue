db70f6109e2bc377c6db15eca0dcf31eeb53ee90
Josh Manjaro <josh.burchdco@gmail.com>
2022-10-19 20:06:16 -0500

    script setup for NutralyzShowcase

--- quasar-project/src/modules/Nutralyz/views/NutralyzShowcase.vue
+++ quasar-project/src/modules/Nutralyz/views/NutralyzShowcase.vue
@@ -2,6 +2,30 @@
   <q-layout class="gradient-prominence-1"> </q-layout>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import ShowcaseCarousel from "src/modules/Common/components/ShowcaseCarousel.vue";
+
+const photos = [
+  "src/assets/img/nutralyz/screen2.png",
+  "src/assets/img/nutralyz/screen3.png",
+  "src/assets/img/nutralyz/screen4.png",
+  "src/assets/img/nutralyz/screen1.png",
+  "src/assets/img/nutralyz/screen5.png",
+  "src/assets/img/nutralyz/screen6.png",
+  "src/assets/img/nutralyz/screen7.png",
+  "src/assets/img/nutralyz/screen8.png",
+];
+
+function handleAppleClick() {
+  window.open("", "_blank");
+}
+
+function handleGoogleClick() {
+  window.open(
+    "https://play.google.com/store/apps/details?id=com.nutralyz",
+    "_blank",
+  );
+}
+</script>
 
 <style></style>
