c1371da78c9680d8b64b10a94a4cf33471447522
Josh Manjaro <josh.burchdco@gmail.com>
2022-10-20 16:40:05 -0500

    initial version of Nutralyz DirectSupport component

---
+++ quasar-project/src/modules/Nutralyz/components/DirectSupport.vue
@@ -0,0 +1,25 @@
+<template>
+  <q-card
+    class="prominence-2 text-prominence-2 shadow-2 rounded-borders-10"
+    style="width: 84vw; max-width: 800px"
+  >
+    <q-card-section class="text-h5 text-weight-bold">
+      For Direct Support:
+    </q-card-section>
+    <q-card-section class="row justify-center q-pt-none">
+      <q-btn
+        push
+        icon="mdi-email"
+        label="support@nutralyz.com"
+        color="info"
+        text-color="white"
+        no-caps
+        href="mailto:support@nutralyz.com?subject=Nutralyz%20Support%20Request"
+      ></q-btn>
+    </q-card-section>
+  </q-card>
+</template>
+
+<script setup lang="ts"></script>
+
+<style></style>
