04def5943717347084f93c169cb246fb952e4252
Josh Manjaro <josh.burchdco@gmail.com>
2022-10-20 16:40:30 -0500

    added DirectSupport and NutralyzFaq to NutralyzSupport

--- quasar-project/src/modules/Nutralyz/views/NutralyzSupport.vue
+++ quasar-project/src/modules/Nutralyz/views/NutralyzSupport.vue
@@ -1,7 +1,20 @@
 <template>
-  <q-layout class="gradient-prominence-1"> </q-layout>
+  <q-layout class="gradient-prominence-1">
+    <!-- Support Email -->
+    <div class="q-px-sm q-pt-lg q-pb-md row full-width justify-center">
+      <DirectSupport></DirectSupport>
+    </div>
+
+    <!-- FAQ -->
+    <div class="q-px-sm q-pt-lg q-pb-md row full-width justify-center">
+      <NutralyzFaq></NutralyzFaq>
+    </div>
+  </q-layout>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import NutralyzFaq from "src/modules/Nutralyz/components/NutralyzFaq.vue";
+import DirectSupport from "src/modules/Nutralyz/components/DirectSupport.vue";
+</script>
 
 <style></style>
