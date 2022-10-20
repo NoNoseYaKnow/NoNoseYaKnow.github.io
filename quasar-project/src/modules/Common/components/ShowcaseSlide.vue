14198b8463923a8441ac93016e481cbe3b4b10dc
Josh Manjaro <josh.burchdco@gmail.com>
2022-10-19 21:06:28 -0500

    initial version of ShowcaseSlide component

---
+++ quasar-project/src/modules/Common/components/ShowcaseSlide.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-carousel-slide :name="name" style="padding: 4px 0px">
+    <div class="fit row justify-center items-center q-pb-sm">
+      <slot></slot>
+    </div>
+  </q-carousel-slide>
+</template>
+
+<script setup lang="ts">
+export interface ShowcaseSlideProps {
+  name: number;
+}
+
+withDefaults(defineProps<ShowcaseSlideProps>(), {
+  name: 0,
+});
+</script>
+
+<style></style>
