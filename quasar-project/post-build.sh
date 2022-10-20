27a3a9169f48c396a4a91be3c5dad0c36030c9dc
Josh Manjaro <josh.burchdco@gmail.com>
2022-10-20 13:06:42 -0500

    went ahead and added remaining files generated by build to post-build.sh

--- quasar-project/post-build.sh
+++ quasar-project/post-build.sh
@@ -2,7 +2,11 @@
 
 rm -r ../assets
 rm ../index.html
+rm ../icons
+rm ../favicon.ico
 
 mv ./dist/spa/assets ../assets
 mv ./dist/spa/index.html ../index.html
+mv ./dist/spa/icons ../icons
+mv ./dist/spa/favicon.ico ../favicon.ico
 
