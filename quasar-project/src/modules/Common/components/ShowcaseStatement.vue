81a5a206f915cdd1b113eff45737f921ee58890f
Josh Manjaro <josh.burchdco@gmail.com>
2022-10-20 14:15:15 -0500

    initial version of ShowcaseStatement component

---
+++ quasar-project/src/modules/Common/components/ShowcaseStatement.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-card
+    class="q-mx-lg prominence-0 rounded-borders-10 shadow-2 bordered text-italic"
+  >
+    <q-card-section class="prominence-0 q-pa-xs"></q-card-section>
+    <q-card-section
+      class="text-h6 prominence-2 text-prominence-3 rounded-borders-10"
+    >
+      <slot></slot>
+    </q-card-section>
+    <q-card-section class="prominence-0 q-pa-xs"></q-card-section>
+  </q-card>
+</template>
+
+<script setup lang="ts"></script>
+
+<style></style>
