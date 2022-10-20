d739bd6a31756e735ee31a26371f0d902a642fd5
Josh Manjaro <josh.burchdco@gmail.com>
2022-10-19 20:02:55 -0500

    initial version of ShowcaseCarousel component

---
+++ quasar-project/src/modules/Common/components/ShowcaseCarousel.vue
@@ -0,0 +1,49 @@
+<template>
+  <q-carousel
+    v-model="slide"
+    animated
+    arrows
+    infinite
+    style="height: 85vh; max-height: 900px"
+    :style="wideShots ? 'height: 80vh; max-height: 800px' : ''"
+    class="transparent"
+    control-color="input-primary"
+    control-type="push"
+  >
+    <q-carousel-slide
+      v-for="(photo, index) in photos"
+      :key="index"
+      :name="index"
+      style="padding: 4px 0px"
+    >
+      <div class="fit row justify-center items-center q-pb-sm">
+        <q-img
+          :ref="!index ? 'first' : ''"
+          :src="photo"
+          :class="
+            wideShots ? 'showcase-screenshot-wide' : 'showcase-screenshot'
+          "
+          fit="contain"
+          class="rounded-borders-20 shadow-2"
+        ></q-img>
+      </div>
+    </q-carousel-slide>
+  </q-carousel>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+export interface ShowcaseCarouselProps {
+  photos: Array<string>;
+  wideShots?: boolean;
+}
+withDefaults(defineProps<ShowcaseCarouselProps>(), {
+  photos: () => [],
+  wideShots: false,
+});
+
+const slide = ref(0);
+</script>
+
+<style></style>
