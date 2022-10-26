7131ee9ffc02d5c5418be3db71efbc0da30c7558
Josh Manjaro <josh.burchdco@gmail.com>
2022-10-26 18:42:15 -0500

    initial content of ShowcaseFeatureList

--- quasar-project/src/modules/Common/components/ShowcaseFeatureList.vue
+++ quasar-project/src/modules/Common/components/ShowcaseFeatureList.vue
@@ -1,7 +1,38 @@
 <template>
-  <div></div>
+  <q-card
+    class="prominence-3 text-prominence-3 shadow-2 rounded-borders-10"
+    style="width: 84vw; max-width: 1000px"
+  >
+    <!-- Title -->
+    <q-card-section
+      class="text-h5 text-weight-bold q-pb-xs"
+      style="text-decoration: underline"
+    >
+      {{ title }}
+    </q-card-section>
+
+    <!-- Features -->
+    <q-card-section>
+      <ShowcaseFeatureSection
+        v-for="(feature, index) in features"
+        :key="index"
+        :feature="feature"
+      ></ShowcaseFeatureSection>
+    </q-card-section>
+  </q-card>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ShowcaseFeature } from "src/modules/Common/components/ShowcaseFeatureSection.vue";
+import ShowcaseFeatureSection from "src/modules/Common/components/ShowcaseFeatureSection.vue";
+
+export interface ShowcaseFeatureListProps {
+  title?: string;
+  features: ShowcaseFeature[];
+}
+withDefaults(defineProps<ShowcaseFeatureListProps>(), {
+  title: "Features & Highlights",
+});
+</script>
 
 <style></style>
