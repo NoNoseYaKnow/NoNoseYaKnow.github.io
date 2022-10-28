9b696c21358d2f26359126c8acaa4d39f706437b
Josh Manjaro <josh.burchdco@gmail.com>
2022-10-28 16:44:14 -0500

    initial commit of InventoryFeatures

---
+++ quasar-project/src/modules/Inventory/components/InventoryFeatures.vue
@@ -0,0 +1,18 @@
+<template>
+  <ShowcaseFeatureList :features="inventoryFeatures"></ShowcaseFeatureList>
+</template>
+
+<script setup lang="ts">
+import ShowcaseFeatureList from "src/modules/Common/components/ShowcaseFeatureList.vue";
+
+import { ShowcaseFeature } from "src/modules/Common/components/ShowcaseFeatureSection.vue";
+
+const inventoryFeatures: ShowcaseFeature[] = [
+  {
+    title: "",
+    highlights: [""],
+  },
+];
+</script>
+
+<style></style>
